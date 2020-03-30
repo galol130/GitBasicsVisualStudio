48679441b1e580323a8fa236da99211a9492ee64
Galo García Pugliese <galo130@hotmail.com>
2020-03-30 12:32:46 +0200

    Primer commit

---
+++ GitBasics/GitBasics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitBasics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
